d948decf47f37c240473b88addbe80b8f42ed164
Aditya Raj <aditya2013raj01@gmail.com>
2022-08-09 13:04:58 +0530

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'dash_board#home'
-  get '/home', to: 'dash_board#home'
+  get '/home',      to: 'dash_board#home'
+  get  '/signup',   to: 'users#new'
 end
