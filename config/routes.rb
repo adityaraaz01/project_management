b0bb4b925ca99af7fbff6da2aad7027a9a86fe83
Aditya Raj <aditya2013raj01@gmail.com>
2022-08-16 17:39:46 +0530

    setting js environment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get    'sessions/new'
+  get    'users/new'
   root   'dash_board#home'
   get    '/home',    to: 'dash_board#home'
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
+ # get    '/logout',  to: 'sessions#destroy'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
